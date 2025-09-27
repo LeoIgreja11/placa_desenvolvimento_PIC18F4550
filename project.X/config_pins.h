8220bc34df45be5a4ed8cf560b37d1b70b00b3ae
Leonardo Igreja <168494287+LeoIgreja11@users.noreply.github.com>
2025-09-26 22:34:12 -0300

    Add files via upload

---
+++ project.X/config_pins.h
@@ -0,0 +1,10 @@
+//Arquivo de configuração dos pinos ligados ao display LCD
+
+#define RS LATEbits.LATE0       //define o pino em que RS está conectado
+#define EN LATEbits.LATE1       //define o pino em que EN está conectado
+#define DB4 LATDbits.LATD4      //define o pido em que D4 está conectado
+#define DB5 LATDbits.LATD5      //define o pino em que D5 está conectado
+#define DB6 LATDbits.LATD6      //define o pino em que D5 está conectado
+#define DB7 LATDbits.LATD7      //define o pino em que D5 está conectado
+
+
